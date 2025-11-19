965406fa1344e6883effd80adafa619b015501d7
Jasky Sor <jaskysor@gmail.com>
2025-11-19 15:42:51 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,20 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Creating restaurants..."
+
+restaurants = [
+  { name: "Epicure", address: "75008 Paris", phone_number: "01 42 00 00 00", category: "french" },
+  { name: "Sushi Shop", address: "Tokyo", phone_number: "+81 3 1234 5678", category: "japanese" },
+  { name: "Pizza Roma", address: "Rome", phone_number: "+39 06 1111 2222", category: "italian" },
+  { name: "Pekin Express", address: "Beijing", phone_number: "+86 10 3333 4444", category: "chinese" },
+  { name: "Frite Corner", address: "Bruxelles", phone_number: "+32 2 555 6666", category: "belgian" }
+]
+
+restaurants.each do |attrs|
+  restaurant = Restaurant.create!(attrs)
+  puts "Created #{restaurant.name}"
+end
+
+puts "Finished! Created #{Restaurant.count} restaurants."
